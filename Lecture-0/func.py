4603f5ca3dc227cb3ac206dd760592f7a7f1e94a
mmyskiv11 <mmykon11@gmail.com>
2023-08-19 15:07:42 +0700

    adding main function that taking effect on hello function - damn confusing tbh

--- Lecture-0/func.py
+++ Lecture-0/func.py
@@ -1,7 +1,10 @@
+def main():
+    name = input("What's your name? ").strip().title()
+    hello(name)
+
+
 def hello(to="world"):
     print("Hello,", to)
 
 
-hello()
-name = input("What's your name? ").strip().title()
-hello(name)
+main()
