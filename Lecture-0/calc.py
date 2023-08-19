6ebe0bc42c0e0254866344281fae82a83543fb38
mmyskiv11 <mmykon11@gmail.com>
2023-08-19 14:22:34 +0700

    Learning float

--- Lecture-0/calc.py
+++ Lecture-0/calc.py
@@ -2,3 +2,8 @@ x = int(input("Enter the value of X: "))
 y = int(input("Enter the value of Y: "))
 
 print(x + y)
+
+a = float(input("Enter the value of A: "))
+b = float(input("Enter the value of B: "))
+
+print(a + b)
