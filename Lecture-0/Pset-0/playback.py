3e3bc8693a41461552b712a6180056f8b68b1b9b
mmyskiv11 <mmykon11@gmail.com>
2023-08-19 15:57:11 +0700

    doing playback problem set

---
+++ Lecture-0/Pset-0/playback.py
@@ -0,0 +1,6 @@
+'''
+Playback Speed - replacing each space with ... (i.e., three periods).
+- https://cs50.harvard.edu/python/2022/psets/0/playback/
+'''
+text = str(input()).replace(" ", "...")
+print(text)
