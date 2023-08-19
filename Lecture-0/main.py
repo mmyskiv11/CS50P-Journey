5eaa0a3ddfb14c331c82d3941cd0f6ef16c75863
mmyskiv11 <mmykon11@gmail.com>
2023-08-19 13:51:07 +0700

    Learning string method

--- Lecture-0/main.py
+++ Lecture-0/main.py
@@ -2,6 +2,7 @@
 '''
 Important Links:
 - https://docs.python.org/
+- https://docs.python.org/3/library/functions.html
 - https://docs.python.org/3/library/functions.html#print
 - https://docs.python.org/3/library/stdtypes.html#string-methods
 
@@ -11,8 +12,9 @@ Lecture Notes:
 Lecture Problem set:
 - https://cs50.harvard.edu/python/2022/psets/0/
 '''
-# Ask user for their name
+# Ask user for their name and fav book
 name = input("What is yout name? ")
+book = input("What is your favorite book title? ").strip().title()
 
 # Say hello to user
 print("Hello,", name)
@@ -22,3 +24,21 @@ print(name)
 print("Hello,", name, sep=":")
 print("Hello, \"Friends\"")  # Escape Sequence
 print(f"Hello, {name}")  # This is the F string
+
+# String Method
+# Remove user's whitespace
+name = name.strip()
+print(f"Hello, {name}")
+
+# Capitalize first letter
+name = name.capitalize()
+print(f"Hello, {name}")
+
+# Capitalizing using title based capitalization
+name = name.title()
+print(f"Hello, {name}")
+
+# Removing Whitespace and capitalize user's name
+name = name.strip().title()
+print(f"Hello, {name}")
+print(f"My favorite book is \"{book}\"")
